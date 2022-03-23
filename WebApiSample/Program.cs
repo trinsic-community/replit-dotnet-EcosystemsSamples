ac74dd69b442fb2a985c6f895e7c1b328529383d
Tomislav Markovski <tmarkovski@gmail.com>
2022-03-23 02:00:52 +0000

    throw error on empty token

--- WebApiSample/Program.cs
+++ WebApiSample/Program.cs
@@ -3,7 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddTrinsic(options => {
   // configure server auth token
-  options.AuthToken = Environment.GetEnvironmentVariable("AUTH_TOKEN");
+  options.AuthToken = Environment.GetEnvironmentVariable("AUTH_TOKEN")
+    ?? throw new("AuthToken not set. Please update the .replit file or set the environment variable AUTH_TOKEN");
 });
 
 var app = builder.Build();
