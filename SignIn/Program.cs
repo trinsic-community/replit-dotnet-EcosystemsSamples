662111e8f28f9bd8baac25593c16348968704820
Tomislav Markovski <tmarkovski@gmail.com>
2022-04-07 15:39:49 +0000

    Updated sign in sample

--- SignIn/Program.cs
+++ SignIn/Program.cs
@@ -4,7 +4,27 @@ var accountService = new AccountService();
 
 var authToken = await accountService.SignInAsync(new());
 
+// to sign in to a specific ecosystem pass the name of that
+// ecosystem to the sign in request
+// 
+// var authToken = await accountService.SignInAsync(new() {
+//   EcosystemId = "ecosystem-name-or-id"
+// });
+
 var infoResponse = await accountService.GetInfoAsync();
 
 Console.WriteLine($"AuthToken = {authToken}");
 Console.WriteLine($"Info = {infoResponse.ToString()}");
+
+// the token will be saved automatically to local storage
+// it can be optionally passed to the services using
+// 
+//   var walletService = new WalletService(new() { 
+//      AuthToken = authToken
+//   });
+
+var walletService = new WalletService();
+
+var search = await walletService.SearchAsync();
+
+Console.WriteLine($"Search = {search}");
